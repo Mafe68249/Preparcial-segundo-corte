dea8d6b8589c62d0fffbbb39755d630f937151ba
Maria Fernanda Hincapie <mfhincapie68@ucatolica.edu.co>
2026-05-20 18:21:14 -0500

    UPDATE

--- database.py
+++ database.py
@@ -8,11 +8,12 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-
 engine = create_engine(
     DATABASE_URL,
-    echo=True
+    pool_pre_ping=True,
+    pool_recycle=300
 )
+
 
 
 def create_tables():
